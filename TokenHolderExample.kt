75398b18f502b1eaddc405c1e460bda82a855b38
yrsegal <yrsegal@gmail.com>
2018-01-26 00:19:36 -0500

    let's heroku this

--- TokenHolderExample.kt
+++
@@ -1,5 +0,0 @@
-package wiresegal.wob
-
-// Copy this file into src/main/java/wiresegal/wob and replace the placeholder token with your own to use this bot.
-
-val token = "Your Discord Bot token goes here."
---
+++ src/main/java/wiresegal/wob/TokenHolder.kt
@@ -0,0 +1,8 @@
+package wiresegal.wob
+
+/**
+ * @author WireSegal
+ * Created at 12:03 AM on 1/26/18.
+ */
+
+val token: String = System.getenv("DISCORD_TOKEN")
