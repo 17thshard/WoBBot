9c3ff77d686dda6d683caa53a1f33f517d36c619
yrsegal <yrsegal@gmail.com>
2018-02-03 20:00:26 -0500

    archive more than just the rattles

--- src/main/java/wiresegal/wob/WordsOfBrandon.kt
+++ src/main/java/wiresegal/wob/WordsOfBrandon.kt
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.node.ObjectNode
 import de.btobastian.javacord.DiscordApi
 import de.btobastian.javacord.DiscordApiBuilder
 import de.btobastian.javacord.entities.DiscordEntity
+import de.btobastian.javacord.entities.channels.TextChannel
 import de.btobastian.javacord.entities.message.Message
 import de.btobastian.javacord.entities.message.embed.EmbedBuilder
 import de.btobastian.javacord.entities.permissions.PermissionState
@@ -25,56 +26,28 @@ import java.util.*
  * Created at 9:12 PM on 1/25/18.
  */
 
-
-// https://discordapp.com/oauth2/authorize?client_id=406271036913483796&scope=bot&permissions=8192
-
 val arcanumColor = Color(0x003A52)
 const val iconUrl = "https://cdn.discordapp.com/emojis/373082865073913859.png?v=1"
-val rattles = arrayOf(
-    "You've killed me. Bastards, you've killed me! While the sun is still hot, I die!" to "Collected on Chachabah 1171, 10 seconds pre-death, by the Silent Gatherers. Subject was a darkeyed soldier thirty-one years of age. Sample is considered questionable",
-    "The love of men is a frigid thing, a mountain stream only three steps from the ice. We are his. Oh Stormfather... we are his. It is but a thousand days, and the Everstorm comes." to "Collected on Shashahes 1171, 31 seconds pre-death, by the Silent Gatherers. Subject was a darkeyed pregnant woman of middle years. The child did not survive",
-    "Ten orders. We were loved, once. Why have you forsaken us, Almighty! Shard of my soul, where have you gone?" to "Collected on Kakan 1171, 5 seconds pre-death, by the Silent Gatherers. Subject was a lighteyed woman in her third decade",
-    "A man stood on a cliffside and watched his homeland fall into dust. The waters surged beneath, so far beneath. And he heard a child crying. They were his own tears." to "Collected on Tanatesev 1171, 30 seconds pre-death, by the Silent Gatherers. Subject was a cobbler of some renown.",
-    "I'm dying, aren't I? Healer, why do you take my blood? Who is that beside you, with his head of lines? I can see a distant sun, dark and cold, shining in a black sky." to "Collected on Jesanach 1172, 11 seconds pre-death, by the Silent Gatherers. Subject was a Reshi chull trainer. Sample is of particular note.",
-    "I have seen the end, and have heard it named. The Night of Sorrows, the True Desolation. The Everstorm." to "Collected on Naneses 1172, 15 seconds pre-death, by the Silent Gatherers. Subject was a darkeyed youth of unknown origin.",
-    "I'm cold. Mother, I'm cold. Mother? Why can I still hear the rain? Will it stop?" to "Collected on Vevishes 1172, 32 seconds pre-death, by the Silent Gatherers. Subject was a lighteyed female child, approximately six years old.",
-    "They are aflame. They burn. They bring the darkness when they come, and so all you can see is that their skin is aflame. Burn, burn, burn..." to "Collected on Palahishev 1172, 21 seconds pre-death, by the Silent Gatherers. Subject was a baker's apprentice.",
-    "Victory! We stand atop the mount! We scatter them before us! Their homes become our dens, their lands are now our farms! And they shall burn, as we once did, in a place that is hollow and forlorn." to "Collected on Ishashan 1172, 18 seconds pre-death, by the Silent Gatherers. Subject was a lighteyed spinster of the eighth dahn.",
-    "Ten people, with Shardblades alight, standing before a wall of black and white and red." to "Collected on Jesachev 1173, 12 seconds pre-death, by the Silent Gatherers. Subject an ardent member of the the Silent Gatherers, overheard during his last moments.",
-    "Three of sixteen ruled, but now the Broken One reigns." to "Collected on Chachanan 1173, 84 seconds pre-death, by the Silent Gatherers. Subject was a cutpurse with the wasting sickness, of partial Iriali descent.",
-    "I'm standing over the body of a brother. I'm weeping. Is that his blood or mine? What have we done?" to "Collected on Vevanev 1173, 107 seconds pre-death, by the Silent Gatherers. Subject was an out-of-work Veden sailor.",
-    "He must pick it up, the fallen title! The tower, the crown, and the spear!" to "Collected on Vevahach 1173, 8 seconds pre-death, by the Silent Gatherers. Subject was a prostitute of unknown background.",
-    "The burdens of nine become mine. Why must I carry the madness of them all? Oh, Almighty, release me." to "Observed on Palaheses 1173, collected secondhand and later reported to the Silent Gatherers. Subject was a wealthy lighteyes.",
-    "A woman sits and scratches out her own eyes. Daughter of kings and winds, the vandal." to "Collected on Palahevan 1173, 73 seconds pre-death, by the Silent Gatherers. Subject was a beggar of some renown, known for his elegant songs.",
-    "Light grows so distant. The storm never stops. I am broken, and all around me have died. I weep for the end of all things. He has won. Oh, he has beaten us." to "Collected on Palahakev 1173, 16 seconds pre-death, by the Silent Gatherers. Subject was a Thaylen sailor.",
-    "I hold the suckling child in my hands, a knife at his throat, and know that all who live wish me to let the blade slip. Spill its blood upon the ground, over my hands, and with it gain us further breath to draw." to "Collected on Shashanan 1173, 23 seconds pre-death, by the Silent Gatherers. Subject was a darkeyed youth of sixteen years. Sample is of particular note.",
-    "Re-Shephir, the Midnight Mother, giving birth to abominations with her essence so dark, so terrible, so consuming. She is here! She watches me die!" to "Collected on Shashabev 1173, 8 seconds pre-death, by the Silent Gatherers. Subject was a darkeyed dock-worker in his forties, father of three.",
-    "The death is my life, the strength becomes my weakness, the journey has ended." to "Observed on Betabanes 1173, 95 seconds pre-death, collected secondhand and later reported to the the Silent Gatherers. Subject was a scholar of some minor renown. Sample considered questionable.",
-    "Above the final void I hang, friends behind, friends before. The feast I must drink clings to their faces, and the words I must speak spark in my mind. The old oaths will be spoken anew." to "Collected on Betabanan 1173, 45 seconds pre-death, by the Silent Gatherers. Subject was a lighteyed child of five years. Diction improved remarkably when giving sample.",
-    "In the storm I awaken, falling, spinning, grieving." to "Collected on Kakanev 1173, 13 seconds pre-death, by the Silent Gatherers. Subject was a city guardsman.",
-    "The darkness becomes a palace. Let it rule! Let it rule!" to "Collected on Kakevah 1173, 22 seconds pre-death, by the Silent Gatherers. Subject was a darkeyed Selay man of unknown profession.",
-    "I wish to sleep. I know now why you do what you do, and I hate you for it. I will not speak of the truths I see." to "Collected on Kakashah 1173, 142 seconds pre-death, by the Silent Gatherers. Subject was a Shin sailor, left behind by his crew, reportedly for bringing them ill luck. Sample largely useless.",
-    "They come from the pit, two dead men, a heart in their hands, and I know that I have seen true glory." to "Collected on Kakashah 1173, 13 seconds pre-death, by the Silent Gatherers. Subject was a rickshaw puller.",
-    "I see them. They are the rocks. They are the vengeful spirits. Eyes of red." to "Collected on Kakakes 1173, 8 seconds pre-death, by the Silent Gatherers. Subject was a darkeyed young woman of fifteen who was reportedly mentally unstable since childhood.",
-    "That chanting, that singing, those rasping voices." to "Collected on Kakatach 1173, 16 seconds pre-death, by the Silent Gatherers. Subject was a middle-aged potter who reported seeing strange dreams during highstorms during the previous two years.",
-    "Let me no longer hurt! Let me no longer weep! Dai-gonarthis! The Black Fisher holds my sorrow and consumes it!" to "Collected on Tanatesach 1173, 28 seconds pre-death, by the Silent Gatherers. Subject was a darkeyed female street juggler.",
-    "They named it the Final Desolation, but they lied. Our gods lied. Oh, how they lied. The Everstorm comes. I hear its whispers, see its stormwall, know its heart." to "Collected on Tanatanes 1173, 8 seconds pre-death, by the Silent Gatherers. Subject was an Azish itinerant worker. Sample of particular note.",
-    "All is withdrawn for me. I stand against the one who saved my life. I protect the one who killed my promises. I raise my hand. The storm responds." to "Collected on Tanatanev 1173, 18 seconds pre-death, by the Silent Gatherers. Subject was a darkeyed mother of four in her sixty-second year.",
-    "Above silence, the illuminating storms—dying storms—illuminate the silence above." to "Collected on Tanatanev 1173, 18 seconds pre-death, by the Silent Gatherers. Subject was an illiterate Herdazian.",
-    "He watches! The Black piper in the night. He holds us in his palm... playing a tune that no man can hear!" to "Observed circa 1172. Subject was Cenn, a member of Kaladin's squad in Brightlord Meridas Amaram's army.",
-    "They break the land itself! They want it, but in their rage they will destroy it. Like the jealous man burns his rich things rather than let them be taken by his enemies! They come!" to "Observed circa ? 1173. Subject was Gadol, a member of Bridge Four.",
-    "And all the world was shattered! The rocks trembled with their steps, and the stones reached toward the heavens. We die! We die!" to "Observed circa ? 1173. Subject was Maps, a member of Bridge Four.",
-    "The day was ours, but they took it. Stormfather! You cannot have it. The day is ours. They come, rasping, and the lights fail. Oh, Stormfather!" to "Collected circa Tanat ? 1173 by Taravangian. Subject was a young boy.",
-    "So the night will reign, for the choice of honor is life..." to "Observed circa Ishi ? 1173 by Taravangian. Subject was King Valam of Jah Keved.")
-
-val rattleEmbeds get() = rattles.mapIndexed { idx, (rattle, comment) -> EmbedBuilder().apply {
-    setTitle("(${idx + 1}/${rattles.size}) \nDeath Rattles")
-    setColor(Color.RED)
-    setDescription(rattle)
-    setFooter(comment)
-} }
 
 val api: DiscordApi = DiscordApiBuilder().setToken(token).login().join()
+
+fun Array<Pair<String, String>>.embeds(title: String, color: Color): List<EmbedBuilder> {
+    return this.mapIndexed { idx, (rattle, comment) -> EmbedBuilder().apply {
+        setTitle("(${idx + 1}/$size) \n$title")
+        setColor(color)
+        setDescription(rattle)
+        setFooter(comment)
+    } }
+}
+
+fun TextChannel.sendRandomEmbed(requester: DiscordEntity, title: String, color: Color, messages: Array<Pair<String, String>>) {
+    val embeds = messages.embeds(title, color)
+
+    val index = (Math.random() * embeds.size).toInt()
+    val embed = embeds[index]
+
+    sendMessage(embed).get().setupDeletable(requester).setupControls(requester, index, embeds)
+}
 
 fun Element.find(vararg evaluators: Evaluator) = allElements.find(*evaluators)
 
@@ -244,6 +217,23 @@ fun Message.setupDeletable(id: Long): Message {
     return this
 }
 
+fun Message.setupControls(requester: DiscordEntity, index: Int, embeds: List<EmbedBuilder>): Message {
+    if (embeds.size > 2)
+        addReaction(first)
+    if (embeds.size > 10)
+        addReaction(jumpLeft)
+    addReaction(arrowLeft)
+    addReaction(done)
+    addReaction(arrowRight)
+    if (embeds.size > 10)
+        addReaction(jumpRight)
+    if (embeds.size > 2)
+        addReaction(last)
+
+    messagesWithEmbedLists[id] = Triple(requester.id, index, embeds)
+    return this
+}
+
 fun search(message: Message, terms: List<String>) {
     val waiting = message.channel.sendMessage("Searching for \"${terms.joinToString().replace("&!", "!")}\"...").get()
     val type = message.channel.typeContinuously()
@@ -258,23 +248,9 @@ fun search(message: Message, terms: List<String>) {
                 finalEmbed.setTitle(finalEmbed.toJsonNode()["title"].asText().replace(".*\n".toRegex(), ""))
                 message.channel.sendMessage(finalEmbed).get().setupDeletable(message.author)
             }
-            else -> {
-                val search = message.channel.sendMessage(allEmbeds.first()).get()
-                search.setupDeletable(message.author)
-                if (allEmbeds.size > 2)
-                    search.addReaction(first)
-                if (allEmbeds.size > 10)
-                    search.addReaction(jumpLeft)
-                search.addReaction(arrowLeft)
-                search.addReaction(done)
-                search.addReaction(arrowRight)
-                if (allEmbeds.size > 10)
-                    search.addReaction(jumpRight)
-                if (allEmbeds.size > 2)
-                    search.addReaction(last)
-
-                messagesWithEmbedLists[search.id] = Triple(message.author.id, 0, allEmbeds)
-            }
+            else ->
+                message.channel.sendMessage(allEmbeds.first()).get()
+                        .setupDeletable(message.author).setupControls(message.author, 0, allEmbeds)
         }
         waiting.delete()
     } catch (e: DiscordException) {
@@ -314,9 +290,10 @@ fun main(args: Array<String>) {
                             "\"" + content + "\"" else content
 
                         val terms = "[\"“]([\\w\\s,+!|&]+)[\"”]".toRegex().findAll(contentModified).toList()
-                                .flatMap { it.groupValues[1]
-                                        .replace("([^&])!".toRegex(), "$1&!")
-                                        .split("[\\s,]+".toRegex())
+                                .flatMap {
+                                    it.groupValues[1]
+                                            .replace("([^&])!".toRegex(), "$1&!")
+                                            .split("[\\s,]+".toRegex())
                                 }.filter { it.matches("[!+|&\\w]+".toRegex()) }
                         if (terms.any()) async {
                             search(message, terms)
@@ -338,7 +315,8 @@ fun main(args: Array<String>) {
                     setColor(arcanumColor)
 
                     setDescription("Author: $wireStr$add\n" +
-                                "[Invite Link]($invite) | " +
+                            "Commands: `!wob` | `Ask the Silent Gatherers` | `Consult the Diagram` | `Check the Gemstone Archives`\n" +
+                            "[Invite Link]($invite) | " +
                             "[Github Source](https://github.com/yrsegal/WoBBot) | " +
                             "[Arcanum](https://wob.coppermind.net/)")
                 })
@@ -350,26 +328,12 @@ fun main(args: Array<String>) {
                     "strengthbeforeweakness" in noChrTrimmed &&
                     "journeybeforedestination" in noChrTrimmed)
                 message.channel.sendMessage("**`These Words are Accepted.`**")
-            else if (noChrTrimmed.startsWith("consultthediagram")) {
-                val index = (Math.random() * rattles.size).toInt()
-                val embed = message.channel.sendMessage(rattleEmbeds[index]).get()
-
-                embed.setupDeletable(message.author)
-
-                if (rattleEmbeds.size > 2)
-                    embed.addReaction(first)
-                if (rattleEmbeds.size > 10)
-                    embed.addReaction(jumpLeft)
-                embed.addReaction(arrowLeft)
-                embed.addReaction(done)
-                embed.addReaction(arrowRight)
-                if (rattleEmbeds.size > 10)
-                    embed.addReaction(jumpRight)
-                if (rattleEmbeds.size > 2)
-                    embed.addReaction(last)
-
-                messagesWithEmbedLists[embed.id] = Triple(message.author.id, index, rattleEmbeds)
-            }
+            else if (noChrTrimmed.startsWith("askthesilentgatherers"))
+                message.channel.sendRandomEmbed(message.author, "Death Rattles", Color.RED, rattles)
+            else if (noChrTrimmed.startsWith("consultthediagram"))
+                message.channel.sendRandomEmbed(message.author, "The Diagram", Color.GREEN, diagram)
+            else if (noChrTrimmed.startsWith("checkthegemstonearchives"))
+                message.channel.sendRandomEmbed(message.author, "Gemstone Archives", Color.BLUE, archive)
         }
     }
     api.addReactionAddListener {
